be749427cc7bff4df53f22f27c3a0ad06fa7a410
rotji <starrotji@gmail.com>
2025-12-13 23:14:06 +0100

    feat: add About page component

---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About: React.FC = () => (
+    <div style={{ padding: '2rem', maxWidth: 700, margin: '0 auto' }}>
+        <h1>About UnitedProfit</h1>
+        <p>UnitedProfit is a platform for creating and joining risk-sharing investment groups. Our mission is to make collaborative investing accessible and transparent for everyone.</p>
+    </div>
+);
+
+export default About;
