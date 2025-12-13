2e87b1f0b20ce4212b11028394fab61af321e743
rotji <starrotji@gmail.com>
2025-12-13 23:13:55 +0100

    feat: add Home page component

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home: React.FC = () => (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Welcome to UnitedProfit</h1>
+        <p>Risk-sharing platform for investment groups.</p>
+    </div>
+);
+
+export default Home;
